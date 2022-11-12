79953024f27a844a3eb8868c79c1cd984bd5d42f
Adam Santillana <adamsantillana@AdamMacMax.local>
2022-11-12 13:31:09 -0600

    Added questions array and updated questions.

--- Assets/script.js
+++ Assets/script.js
@@ -18,6 +18,58 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
     // When user clicks on the submit button, shows results.
     submitButton.onclick = function() {
         showResults(questions, quizContainer, resultsContainer);
-    }
-    
+    }   
 }
+
+var myQuestions = [
+    {
+        question: "Commonly used data types DO Not Include: ",
+        answers: {
+            a: 'strings',
+            b: 'booleans',
+            c: 'alerts',
+            d: 'numbers'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with: ",
+        answers: {
+            a: 'quotes',
+            b: 'curly brackets',
+            c: 'parenthesis',
+            d: 'square brackets'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: {
+            a: 'JavaScript',
+            b: 'terminal/bash',
+            c: 'for loops',
+            d: 'console.log'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: {
+            a: 'JavaScript',
+            b: 'terminal/bash',
+            c: 'for loops',
+            d: 'console.log'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: {
+            a: 'JavaScript',
+            b: 'terminal/bash',
+            c: 'for loops',
+            d: 'console.log'
+        },
+        correctAnswer: 'd'
+    }
+];
